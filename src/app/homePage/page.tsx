a8b1207b087b0697a40817ac034f3ef11d8cda76
cynthialin1228 <cynthialin1228@gmail.com>
2023-12-30 18:55:15 +0800

    homepage color, delete openapi

--- src/app/homePage/page.tsx
+++ src/app/homePage/page.tsx
@@ -5,7 +5,6 @@ import { getProjects, getWritings } from "./actions";
 import { auth } from "@/lib/auth";
 import SignOutButton from "./_components/SignOutButton";
 import Rank from "./_components/rank";
-
 export default async function ProjectsPage() {
   const session = await auth();
   const userId = session?.user?.id;
@@ -16,29 +15,34 @@ export default async function ProjectsPage() {
   const writings = await getWritings(userId);
 
   return (
-    <main className="h-full border font-serif bg-dark-blue text-white"> {/* Set dark blue background and white text color */}
-      <div className="bg-white"> {/* Set white background */}
+    <main className="h-full border font-serif">
+      <div>
         <div className="flex h-10 items-center py-8 pt-8 my-8">
           <Link href="/homePage">
             <h2 className="flex w-screen text-5xl font-bold items-center justify-center">
               GRE StudEE
             </h2>
+            {/* <Separator className="my-3 w-screen" /> */}
           </Link>
         </div>
 
-        <div className="flex gap-6 fixed top-2.5 right-5 text-white"> {/* Set white text color */}
+        <div className="flex gap-6 fixed top-2.5 right-5">
           <div className="flex items-center gap-2">
             <span className="text-md font-semibold">
-              {session?.user?.name || "User"}
+              {
+                session?.user?.name || "User"
+              }
             </span>
           </div>
           <SignOutButton />
         </div>
 
+        
         <div className="flex p-4">
-          <div className="flex-1 p-4 rounded-xl border border-dark-blue bg-white shadow-md"> {/* Set dark blue border, white background, and add shadow */}
+          <div className="flex-1 p-4 rounded-xl border border-black">
+            {/* <h2>Column 1</h2> */}
             <Link
-              className="mx-5 rounded-xl bg-dark-blue px-4 py-2 text-lg transition-all hover:bg-dark-blue-light"
+              className="mx-5 rounded-xl bg-blue-100 px-4 py-2 text-lg drop-shadow-md transition-all hover:bg-gray-300"
               href="/projects/create"
             >
               + 今日單字
@@ -46,30 +50,33 @@ export default async function ProjectsPage() {
             <section className="flex grow flex-col gap-2 my-4">
               {projects.length === 0 ? (
                 <div className="flex h-10 w-full flex-col items-center justify-between p-2">
-                  <h3 className="text-gray-500">No Lists</h3> {/* Set a cold tone text color */}
+                  <h3>No Lists</h3>
                 </div>
               ) : (
-                projects.map((project) => (
-                  <Link
-                    href={`/projects/${project.id}`}
-                    key={project.id}
-                    className="flex w-full cursor-pointer rounded-xl p-2 pl-6 transition-all hover:bg-gray-200"
-                  >
-                    <h3 className="text-xl text-gray-700">{project.name}</h3> {/* Set a cold tone text color */}
-                  </Link>
-                ))
+                projects.map((project) => {
+                  return (
+                    <Link
+                      href={`/projects/${project.id}`}
+                      key={project.id}
+                      className="flex w-full cursor-pointer rounded-xl p-2 pl-6 transition-all hover:bg-gray-200"
+                    >
+                      <h3 className="text-xl">{project.name}</h3>
+                    </Link>
+                  );
+                })
               )}
             </section>
           </div>
 
-          <div className="flex-1 p-4 bg-dark-blue shadow-md rounded-md"> {/* Set dark blue background and add shadow */}
+          <div className="flex-1 p-4">
             {/* <h2>Column 2</h2> */}
             <Rank />
           </div>
 
-          <div className="flex-1 p-4 rounded-xl border border-dark-blue bg-white shadow-md"> {/* Set dark blue border, white background, and add shadow */}
+          <div className="flex-1 p-4 rounded-xl border border-black">
+            {/* <h2>Column 3</h2> */}
             <Link
-              className="mx-2 rounded-xl bg-dark-blue px-4 py-2 text-lg transition-all hover:bg-dark-blue-light"
+              className="mx-2 rounded-xl bg-blue-100 px-4 py-2 text-lg drop-shadow-md transition-all hover:bg-gray-300"
               href="/writings/create2write"
             >
               + 寫作練習
@@ -77,18 +84,20 @@ export default async function ProjectsPage() {
             <section className="flex grow flex-col gap-2 my-4">
               {writings.length === 0 ? (
                 <div className="flex h-10 w-full flex-col items-center justify-between p-2">
-                  <h3 className="text-gray-500">No Writings</h3> {/* Set a cold tone text color */}
+                  <h3>No Writings</h3>
                 </div>
               ) : (
-                writings.map((writing) => (
-                  <Link
-                    href={`/writings/${writing.id}`}
-                    key={writing.id}
-                    className="flex w-full cursor-pointer rounded-xl p-2 pl-6 transition-all hover:bg-gray-200 max-w-md overflow-hidden"
-                  >
-                    <h3 className="text-xl text-gray-700 text-overflow-ellipsis overflow-ellipsis whitespace-nowrap">{writing.name}</h3> {/* Set a cold tone text color */}
-                  </Link>
-                ))
+                writings.map((writing) => {
+                  return (
+                    <Link
+                      href={`/writings/${writing.id}`}
+                      key={writing.id}
+                      className="flex w-full cursor-pointer rounded-xl p-2 pl-6 transition-all hover:bg-gray-200 max-w-md overflow-hidden"
+                    >
+                      <h3 className="text-xl text-overflow-ellipsis overflow-ellipsis whitespace-nowrap">{writing.name}</h3>
+                    </Link>
+                  );
+                })
               )}
             </section>
           </div>
--- src/app/writings/[writingId]/actions.ts
+++ src/app/writings/[writingId]/actions.ts
@@ -11,11 +11,6 @@ import {  usersToWritingTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 import { publicEnv } from "@/lib/env/public";
 
-import OpenAI from "openai";
-
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_KEY, // defaults to process.env["OPENAI_API_KEY"]
-});
 
 export async function getWriting(writingId: string) {
   const session = await auth();
@@ -71,62 +66,4 @@ export async function deleteWriting(writingId: string) {
     .returning();
 
   revalidatePath(`/homePage`);
-}
-
-const ChatGPTJudgeSchema = z.object({
-  writingId: z.string(),
-  writingTitle: z.string(),
-  writingContent: z.string(),
-});
-
-export async function ChatGPTJudge(
-  writingId: string,
-  writingTitle: string,
-  writingContent: string | null,
-) {
-  ChatGPTJudgeSchema.parse({
-    writingId,
-    writingTitle,
-    writingContent,
-  });
-
-  const session = await auth();
-  const userId = session?.user?.id;
-  if (!userId) {
-    redirect(`${publicEnv.NEXT_PUBLIC_BASE_URL}`);
-  }
-
-  // const message = "Title: " + writingTitle + "\n" + "Content: " + writingContent + "\n" + "Judge: ";
-  const message = "Title";
-  const apiKey = openai.apiKey;
-  const apiUrl = 'https://api.openai.com/v1/chat/completions'; 
-  const input = {
-    model: "gpt-3.5-turbo-instruct",
-    messages: [
-      // { role: 'system', content: 'You are a helpful judge.' },
-      { role: 'user', content: message },
-      // { role: 'assistant', content: 'Hi there! How can I help you today?' },
-    ],
-  };
-  fetch(apiUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${apiKey}`,
-    },
-    body: JSON.stringify(input),
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log('API Response:', data);
-      // Handle the response data as needed
-      return data;
-    })
-    .catch(error => {
-      console.error('Error:', error);
-      // Handle errors
-      return error;
-    });
-
-  // revalidatePath(`/homePage`);
 }
