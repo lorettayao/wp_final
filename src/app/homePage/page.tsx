384bdd5d45a3046e027ee29e1e85fc8aed2eb89b
lorettayao <a0910330206@gmail.com>
2023-12-30 16:27:03 +0800

    the bg color change

--- src/app/homePage/page.tsx
+++ src/app/homePage/page.tsx
@@ -5,6 +5,7 @@ import { getProjects, getWritings } from "./actions";
 import { auth } from "@/lib/auth";
 import SignOutButton from "./_components/SignOutButton";
 import Rank from "./_components/rank";
+
 export default async function ProjectsPage() {
   const session = await auth();
   const userId = session?.user?.id;
@@ -15,11 +16,11 @@ export default async function ProjectsPage() {
   const writings = await getWritings(userId);
 
   return (
-    <main className="h-full border font-serif">
+    <main className="h-full border font-serif bg-gray-200"> {/* Set a cold tone background color */}
       <div>
         <div className="flex h-10 items-center py-8 pt-8 my-8">
           <Link href="/homePage">
-            <h2 className="flex w-screen text-5xl font-bold items-center justify-center">
+            <h2 className="flex w-screen text-5xl font-bold items-center justify-center text-blue-500"> {/* Set a cold tone text color */}
               GRE StudEE
             </h2>
             {/* <Separator className="my-3 w-screen" /> */}
@@ -28,7 +29,7 @@ export default async function ProjectsPage() {
 
         <div className="flex gap-6 fixed top-2.5 right-5">
           <div className="flex items-center gap-2">
-            <span className="text-md font-semibold">
+            <span className="text-md font-semibold text-gray-700"> {/* Set a cold tone text color */}
               {
                 session?.user?.name || "User"
               }
@@ -37,10 +38,8 @@ export default async function ProjectsPage() {
           <SignOutButton />
         </div>
 
-        
         <div className="flex p-4">
-          <div className="flex-1 p-4 rounded-xl border border-black">
-            {/* <h2>Column 1</h2> */}
+          <div className="flex-1 p-4 rounded-xl border border-gray-300 bg-white shadow-md"> {/* Set a cold tone background color and add shadow */}
             <Link
               className="mx-5 rounded-xl bg-blue-100 px-4 py-2 text-lg drop-shadow-md transition-all hover:bg-gray-300"
               href="/projects/create"
@@ -50,7 +49,7 @@ export default async function ProjectsPage() {
             <section className="flex grow flex-col gap-2 my-4">
               {projects.length === 0 ? (
                 <div className="flex h-10 w-full flex-col items-center justify-between p-2">
-                  <h3>No Lists</h3>
+                  <h3 className="text-gray-500">No Lists</h3> {/* Set a cold tone text color */}
                 </div>
               ) : (
                 projects.map((project) => {
@@ -60,7 +59,7 @@ export default async function ProjectsPage() {
                       key={project.id}
                       className="flex w-full cursor-pointer rounded-xl p-2 pl-6 transition-all hover:bg-gray-200"
                     >
-                      <h3 className="text-xl">{project.name}</h3>
+                      <h3 className="text-xl text-gray-700">{project.name}</h3> {/* Set a cold tone text color */}
                     </Link>
                   );
                 })
@@ -68,13 +67,12 @@ export default async function ProjectsPage() {
             </section>
           </div>
 
-          <div className="flex-1 p-4">
+          <div className="flex-1 p-4 bg-dark-blue rounded-md"> {/* Set a cold tone background color and add shadow */}
             {/* <h2>Column 2</h2> */}
             <Rank />
           </div>
 
-          <div className="flex-1 p-4 rounded-xl border border-black">
-            {/* <h2>Column 3</h2> */}
+          <div className="flex-1 p-4 rounded-xl border border-gray-300 bg-white shadow-md"> {/* Set a cold tone background color and add shadow */}
             <Link
               className="mx-2 rounded-xl bg-blue-100 px-4 py-2 text-lg drop-shadow-md transition-all hover:bg-gray-300"
               href="/writings/create2write"
@@ -84,7 +82,7 @@ export default async function ProjectsPage() {
             <section className="flex grow flex-col gap-2 my-4">
               {writings.length === 0 ? (
                 <div className="flex h-10 w-full flex-col items-center justify-between p-2">
-                  <h3>No Writings</h3>
+                  <h3 className="text-gray-500">No Writings</h3> {/* Set a cold tone text color */}
                 </div>
               ) : (
                 writings.map((writing) => {
@@ -94,7 +92,7 @@ export default async function ProjectsPage() {
                       key={writing.id}
                       className="flex w-full cursor-pointer rounded-xl p-2 pl-6 transition-all hover:bg-gray-200 max-w-md overflow-hidden"
                     >
-                      <h3 className="text-xl text-overflow-ellipsis overflow-ellipsis whitespace-nowrap">{writing.name}</h3>
+                      <h3 className="text-xl text-gray-700 text-overflow-ellipsis overflow-ellipsis whitespace-nowrap">{writing.name}</h3> {/* Set a cold tone text color */}
                     </Link>
                   );
                 })
